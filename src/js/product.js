bb6c6098fd24169a36da116c77254ff54a7e7527
DC-CC-21 <handstands1000@gmail.com>
2024-04-25 16:35:55 -0400

    completed Add Item to Cart Broken task

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,25 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  //> Get previous cart contents from localstorage
+  let cartContents = getLocalStorage("so-cart");
+
+  //> Check if cartContens is not null
+  if (cartContents) {
+    if (cartContents instanceof Array) {
+      //> If cartContents is an Array, add previous and new contents to localstorage
+      setLocalStorage("so-cart", [...cartContents, product]);
+    } else {
+      //> If cartContents is an object, add the previous object and new object to an array
+      setLocalStorage("so-cart", [cartContents, product]);
+    }
+  } else {
+    //> If cartContents is null, just add the new product to localstorage
+    setLocalStorage("so-cart", [product]);
+  }
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
