7dc55db8fe95e4c5158dd785d46bd20a39e28fda
gianvin <ginamangahassanchezong@gmail.com>
2024-04-27 17:05:39 +0800

    undo changes on async function add to cart handler

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
 
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
