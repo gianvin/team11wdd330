7988ab10ceb4c95c3e28bf331e0b48744880567b
gianvin <ginamangahassanchezong@gmail.com>
2024-04-27 17:00:46 +0800

    edited the addproduct to cart function

--- src/js/product.js
+++ src/js/product.js
@@ -4,6 +4,9 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
+  let cart = getLocalStorage("so-cart");
+  if (cart === null) cart = [];
+  cart.push(product);
   setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
