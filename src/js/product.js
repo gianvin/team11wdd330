cdd980e37b2595eaf705f7f11e368492b13f91f8
gianvin <ginamangahassanchezong@gmail.com>
2024-05-02 04:16:09 +0800

    created the files for week 2 group activity.

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,14 @@
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
   let cart = getLocalStorage("so-cart");
