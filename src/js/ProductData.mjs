c729605cb5eb3f95ee48bbc12b50e8b7d18390ad
gianvin <ginamangahassanchezong@gmail.com>
2024-05-04 12:35:41 +0800

    edited the product Datamjs based on console srrors

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,7 +9,7 @@ function convertToJson(res) {
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `./json/${this.category}.json`;
+    this.path = `../json/${this.category}.json`;
   }
   async getData() {
     const res = await fetch(this.path);
