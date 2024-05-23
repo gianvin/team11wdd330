608709a5ff3cbfbfdd34005ce0e09190e129093e
rafigarcia <rafaela.garcia.angulo@gmail.com>
2024-05-22 20:38:18 -0500

    W04

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,12 +1,12 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
-import { getParam } from "./utils.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+
+loadHeaderFooter();
 
 const listEl = document.querySelector(".product-list");
 const category = getParam("category");
-
 const dataSource = new ProductData();
 const productListing = new ProductListing(category, dataSource, listEl);
 
-document.querySelector(".top-products").innerHTML = `Top Products: ${category}`;
 productListing.init();
